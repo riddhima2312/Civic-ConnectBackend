2691107607416c6b5c935e8debe27d900b25841b
Riddhima <riddhimakgupta@gmail.com>
2026-04-28 13:31:14 +0530

    added test controller

---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend is LIVE 🚀";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
